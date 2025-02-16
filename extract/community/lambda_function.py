71570ecd01968cb69fdc4eb28b99858420d64711
Junho Lee <100112445+Junoritto@users.noreply.github.com>
2025-02-16 23:00:35 +0900

    feat: dcinside 크롤러 구현 + 커뮤니티 크롤러 폴더 구조 변경 (#20)

--- extract/community/lambda_function.py
+++ extract/community/lambda_function.py
@@ -6,7 +6,8 @@ import pandas as pd
 import pyarrow
 import pyarrow.parquet as pq
 
-from bobae.bobae_crawler import BobaeCrawler
+from bobae_crawler import BobaeCrawler
+from dcinside_crawler import DCInsideCrawler
 from type.community_crawler import CommunityRequest, CommunityResponse
 from conf import BUCKET_NAME, BOBAEDREAM_URL
 
@@ -15,11 +16,9 @@ def upload_df_to_s3(df, bucket_name, object_name):
 
     # 데이터프레임을 parquet로 변환하여 메모리에서 처리
     parquet_buffer = io.BytesIO()
-
     # Pyspark에서 datetime이 깨지기 때문에 us 단위로 변환하여 저장
-    table = pyarrow.Table.from_pandas(df=df)
+    table = pyarrow.Table.from_pandas(df=df, preserve_index=False)
     pq.write_table(table, parquet_buffer, coerce_timestamps='us')
-    
     parquet_buffer.seek(0)
 
     try:
@@ -40,68 +39,45 @@ def lambda_handler(event, context):
                                    start_time=datetime.strptime(event.get('start_time_str'), "%Y-%m-%dT%H:%M"), 
                                    end_time=datetime.strptime(event.get('end_time_str'), "%Y-%m-%dT%H:%M")) 
         
-        community = 'bobaedream'
+        community = event.get('community')
         keyword = request['keyword']
-        start_datetime = request['start_time']
-        end_datetime = request['end_time']
+        start_time = request['start_time']
+        end_time = request['end_time']
 
 
-        ### 파라미터 확인 ###
-        # community = event.get('community')
-        # keyword = event.get('keyword')
-        # start_datetime = datetime.strptime(event.get('start_time_str'), "%Y-%m-%dT%H:%M")
-        # end_datetime = datetime.strptime(event.get('end_time_str'), "%Y-%m-%dT%H:%M")
-
-
-        # if not community: return {'statusCode': 400, 'body': json.dumps('Error: community is not specified.')}
+        if not community: return {'statusCode': 400, 'body': json.dumps('Error: community is not specified.')}
         if not keyword: return {'statusCode': 400, 'body': json.dumps('Error: Keyword is not specified.')}
-        if not start_datetime: return {'statusCode': 400, 'body': json.dumps('Error: Start datetime is not specified.')}
-        if not end_datetime: return {'statusCode': 400, 'body': json.dumps('Error: End datetime is not specified.')}
+        if not start_time: return {'statusCode': 400, 'body': json.dumps('Error: Start datetime is not specified.')}
+        if not end_time: return {'statusCode': 400, 'body': json.dumps('Error: End datetime is not specified.')}
+
+        # 크롤러 선택 및 실행
+        if community == 'bobaedream':
+            crawler = BobaeCrawler(request)
+        elif community == 'dcinside':
+            crawler = DCInsideCrawler(request)
+        else:
+            return {'statusCode': 400, 'body': json.dumps(f"Error: Unsupported community '{community}'")}
         
-        
-
-        ### 크롤링 ###
-        crawler = BobaeCrawler(request)
-        results = []
-        current_page = 1
-        stop_crawling = False
-
-        while not stop_crawling:
-            search_url = f"{BOBAEDREAM_URL}&s_key={keyword}&page={current_page}"
-            article_data_list = crawler.process_page(request, current_page, search_url)
-
-            if not article_data_list:
-                break  # 더 이상 게시글이 없으면 크롤링 종료
-
-            for post_url, title, start_datetime, end_datetime in article_data_list:
-                data = crawler.extract_article_data(request, title, post_url)
-
-                if data == 'STOP':
-                    stop_crawling = True
-                    break
-                elif data:
-                    results.append(data)  # CommunityResponse 객체를 JSON으로 변환
-
-            current_page += 1
-
-        # 결과를 DataFrame으로 변환
-        df = pd.DataFrame(results)
+        df = crawler.start_crawling()
 
         if df.empty:
-            return {'statusCode': 500, 'body': json.dumps(f"[ERROR] No data collected for {keyword}.")}
-        
-        ### S3 업로드 ###
-        object_key = f"data/community/{start_datetime.strftime('%Y-%m-%d %H:%M:%S')}_{end_datetime.strftime('%Y-%m-%d %H:%M:%S')}/{start_datetime.strftime('%Y-%m-%d %H:%M:%S')}_{end_datetime.strftime('%Y-%m-%d %H:%M:%S')}_{community}_{keyword}.parquet"
+            return {'statusCode': 500, 'body': json.dumps(f"[ERROR] No data collected for {request['keyword']}.")}
+
+        # S3 업로드
+        object_key = f"data/community/{start_time.strftime('%Y-%m-%d %H:%M:%S')}_{end_time.strftime('%Y-%m-%d %H:%M:%S')}/{start_time.strftime('%Y-%m-%d %H:%M:%S')}_{end_time.strftime('%Y-%m-%d %H:%M:%S')}_{community}_{keyword}.parquet"
+
         upload_result, msg = upload_df_to_s3(df, BUCKET_NAME, object_key)
-        if upload_result == False:
+
+        if not upload_result:
             return {'statusCode': 500, 'body': json.dumps(f"[ERROR] Failed to load at S3\n{msg}")}
-        else:
-            done_key = f"data/community/marker/{community}.done"
-            upload_df_to_s3(pd.DataFrame([{'keyword': keyword, 'start_time': start_datetime, 'end_time': end_datetime}]), BUCKET_NAME, done_key)
+
+        # 크롤링 완료 마커 업로드
+        done_key = f"data/community/marker/{community}.done"
+        upload_df_to_s3(pd.DataFrame([{'keyword': request['keyword'], 'start_time': request['start_time'], 'end_time': request['end_time']}]), BUCKET_NAME, done_key)
 
         return {'statusCode': 200, 'body': json.dumps(msg)}
-
+    
     except ValueError as ve:
         return {'statusCode': 400, 'body': json.dumps(f"[ERROR] {str(ve)}")}
     except Exception as e:
-        return {'statusCode': 500, 'body': json.dumps(f"[ERROR] Unknown error occured. {str(e)}")}
+        return {'statusCode': 500, 'body': json.dumps(f"[ERROR] Unknown error occurred. {str(e)}")}
