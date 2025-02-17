8c977ed6c2a0c1485cc822aaa72f036349be41d0
seonwoonam <potatomoney716@gmail.com>
2025-02-17 17:50:59 +0900

    Feat: Extract 구조 변경 및 lambda함수 수정 (#23)
    * Feat: Extract 구조 변경 및 lambda함수 수정
    
    * Delete: 겹치는 Base crawler 파일 제거

--- extract/news/lambda_function.py
+++ extract/news/lambda_function.py
@@ -1,64 +1,38 @@
 from datetime import datetime
-import sys
-from type.news_crawler import NewsRequest
-from sbs_crawler import SBSCrawler
-from kbs_crawler import KBSCrawler
-from yna_crawler import YNACrawler
 import json
-import threading
 import boto3
 
-NEWS_SOURCES = ["kbs", "sbs", "yna"]
-
+NEWS_SOURCES = ["kbs", "sbs", "yna", "ytn"]
+lambda_client = boto3.client('lambda', region_name='ap-northeast-2')
 
 def lambda_handler(event, context):
     try:
-        keyword = event.get('keyword')
-        start_time = datetime.strptime(event.get('start_time_str'), "%Y-%m-%dT%H:%M")
-        end_time = datetime.strptime(event.get('end_time_str'), "%Y-%m-%dT%H:%M")
-        request: NewsRequest = {
-            "keyword": keyword,
-            "start_time": start_time,
-            "end_time": end_time
-        }
+        start_time = event.get('start_time_str')
+        end_time = event.get('end_time_str')
 
-        if not keyword: return {'statusCode': 400, 'body': json.dumps('Error: Missing Arguments - Keyword.')}
         if not start_time: return {'statusCode': 400, 'body': json.dumps('Error : Missing Arguments - Start_time.')}
         if not end_time: return {'statusCode': 400, 'body': json.dumps('Error : Missing Arguments - End_time.')}
 
-        threads = []
-        results = {}
-
         for source in NEWS_SOURCES:
-            if source == "kbs":
-                kbs_crawler = KBSCrawler(request=request)
-                thread = threading.Thread(target=kbs_crawler.run)
-                thread.start()
-                threads.append(thread)
+            request = {
+                "start_time_str": start_time,
+                "end_time_str": end_time,
+                "source" : source
+            }
+            trigger_news_crawler(request)
 
-            if source == "sbs":
-                sbs_crawler = SBSCrawler(request=request)
-                thread = threading.Thread(target=sbs_crawler.run)
-                thread.start()
-                threads.append(thread)
 
-            if source == "yna":
-                yna_crawler = YNACrawler(request=request)
-                thread = threading.Thread(target=yna_crawler.run)
-                thread.start()
-                threads.append(thread)
-
-        for thread in threads:
-            thread.join()
-
-        trigger_emr()
-
-        return {'statusCode': 200, 'body': json.dumps(results)}
+        return {'statusCode': 200, 'body': json.dumps("All news crawlers triggered asynchronously")}
     except ValueError as valueError:
         return {'statusCode': 400, 'body': json.dumps(str(valueError))}
     except Exception as e:
         return {'statusCode': 500, 'body': json.dumps(f"Error: Unknown error occured. {str(e)}")}
 
+def trigger_news_crawler(news_event):
+    response = lambda_client.invoke(
+        FunctionName='news_extract_function',  
+        InvocationType='Event',             
+        Payload=json.dumps(news_event)
+    )
 
-def trigger_emr():
-    pass
+    print(news_event["source"]," Triggered with status:", response.get("StatusCode"))
