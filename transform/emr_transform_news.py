56066b56e1c031aae5b62c20364c705d46800287
sumin <cleonn7878@gmail.com>
2025-02-18 09:27:56 +0900

    Feat: 뉴스 EMR 결과가 news_score를 저장하도록 구현

--- transform/emr_transform_news.py
+++ transform/emr_transform_news.py
@@ -44,7 +44,7 @@ def transform(data_source:str, output_uri:str, batch_period:str)-> None:
         df_exploded = df.withColumn("accident", F.explode(F.col("accident"))) \
             .filter(F.col("accident").isNotNull())
 
-        res_df = df_exploded.groupBy("keyword", "accident").agg(
+        grouped_df = df_exploded.groupBy("keyword", "accident").agg(
             F.count("*").alias("count"),
             F.to_json(
                 F.collect_list(
@@ -53,16 +53,34 @@ def transform(data_source:str, output_uri:str, batch_period:str)-> None:
             ).alias("news")
         )
 
-        res_df = res_df.select('keyword', 'accident', 'count', 'news')
-        res_df.show()
+        grouped_df = grouped_df.select('keyword', 'accident', 'count', 'news')
+        grouped_df.show()
 
-        res_df.rdd.foreachPartition(partial(update_table_from_batch, param = conf.RDS_PROPERTY, batch_period = batch_period))
-        res_df.coalesce(1).write.mode('overwrite').parquet(output_uri + batch_period + '/')
+        score_schema = grouped_df.schema.add('news_score', IntegerType(), True)
+        score_rdd = grouped_df.rdd.mapPartitions(partial(community_score_rdd_generator, param = conf.RDS_PROPERTY))
+        score_rdd.foreachPartition(partial(merge_batch_into_main_table, param = conf.RDS_PROPERTY, batch_period = batch_period))
+        score_df = spark.createDataFrame(score_rdd, score_schema)
+        score_df.coalesce(1).write.mode('overwrite').parquet(output_uri + batch_period + '/')
+        score_df.show()
         update_main_table(batch_period)
 
     return
 
-def update_table_from_batch(partition, param, batch_period:str):
+def community_score_rdd_generator(partition, param):
+    for row in partition:
+        row_with_score = row.asDict().copy()
+        try:
+            row_with_score['news_score'] = get_community_score_from_gpt()
+            yield row_with_score
+        except Exception as e:
+            print(f"Error executing query: {e}")
+            row_with_score['news_score'] = None
+            yield row_with_score
+
+def get_community_score_from_gpt()->int:
+    return 1
+
+def merge_batch_into_main_table(partition, param, batch_period:str):
     conn = psycopg2.connect(
         dbname= param["dbname"],
         user=param["user"],
@@ -78,6 +96,7 @@ def update_table_from_batch(partition, param, batch_period:str):
         accident = row['accident']
         batch_count = row['count']
         news_json = row['news']
+        news_score = row['news_score']
         query = f"""
         UPDATE accumulated_table
         SET start_batch_time = CASE
@@ -89,7 +108,8 @@ def update_table_from_batch(partition, param, batch_period:str):
         news = jsonb_set(
             news,
             '{{news}}',
-            COALESCE(news->'news', '[]'::jsonb) || '{news_json}'::jsonb )
+            COALESCE(news->'news', '[]'::jsonb) || '{news_json}'::jsonb ) ,
+        news_score = {news_score}
         WHERE car_model = '{car_model}'
             AND accident = '{accident}';
         """
